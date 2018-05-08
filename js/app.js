39165d738291a546291a5c9d11c3a8eb1ae1ed22
Panu Photjanataree TLNW <panup@magnecomp.com>
2018-05-08 15:24:17 +0700

    add controller

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 (function(){
 	'use strict';
 	var app = angular.module('application',[]);//ชื่อโมดูล, โมดูลที่เกี่ยวข้อง (จะกล่าวถึงในภายหลัง)
-	})();//Best Practice for Javascript
+	
+	app.controller('storeController',function(){
+		this.sushi = {
+			name: 'Panu',
+			price:200,
+			description: 'พจนธารี'
+			};
+	});
+	
+})();//Best Practice for Javascript
+	
+	
